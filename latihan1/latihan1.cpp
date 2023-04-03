fc48140ae38b823faf5d26105b2c680c1740f588
alfinur154 <alfinur27@gmail.com>
2023-04-03 11:29:43 +0700

    membuat progam atribut

--- latihan1/latihan1.cpp
+++ latihan1/latihan1.cpp
@@ -14,3 +14,23 @@ struct Mahasiswa
 	AlamatDetail alamat;
 	int umur;
 };
+
+
+int main() {
+	Mahasiswa mhs[3];
+
+	for (int i = 0; i < 3; i++) {
+		cout << "Masukkan NIM :";
+		cin.getline(mhs[i].NIM, 12);
+		cout << "Masukkan nama :";
+		cin.getline(mhs[i].nama, 25);
+		cout << "Alamat :";
+		cout << "\n\tMasukan Desa :";
+		cin.getline(mhs[i].alamat.desa, 20);
+		cout << "\Masukan kota :";
+		cin.getline(mhs[i].alamat.kota, 20);
+		cout << "Masukan umur :";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+	}
+}
