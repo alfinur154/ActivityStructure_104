74ae7b34e55bf861bd52e4af909aa28456140b58
alfinur154 <alfinur27@gmail.com>
2023-04-03 11:16:13 +0700

    membuat detail alamat dan nama

---
+++ latihan1/latihan1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct AlamatDetail
+{
+	char desa[20];
+	char kota[20];
+};
+
+struct Mahasiswa
+{
+	char NIM[12];
+	char nama[25];
+	AlamatDetail alamat;
+	int umur;
+};
