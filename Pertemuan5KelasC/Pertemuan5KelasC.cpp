ccb09340069b5b8823018af27223942f4c77fc0b
alfinur154 <alfinur27@gmail.com>
2023-04-03 10:42:55 +0700

    menampilkan output kelayar

--- Pertemuan5KelasC/Pertemuan5KelasC.cpp
+++ Pertemuan5KelasC/Pertemuan5KelasC.cpp
@@ -19,12 +19,22 @@ int main() {
 	mhs1.umur = 20;
 
 	cout << "Masukkan NIM :";
-	cin >> mhs1.NIM;
+	cin >> mhs2.NIM;
 	cout << "Masukkan Nama :";
-	cin >> mhs1.nama;
+	cin >> mhs2.nama;
 	cout << "Masukkan Alamat :";
-	cin >> mhs1.alamat;
+	cin >> mhs2.alamat;
 	cout << "Masukkan Umur :";
-	cin >> mhs1.umur;
+	cin >> mhs2.umur;
+
+	cout << "\nNIM = " << mhs1.NIM;
+	cout << "\nNama = " << mhs1.nama;
+	cout << "\nAlamat = " << mhs1.alamat;
+	cout << "\nUmur = " << mhs1.umur;
+
+	cout << "\n\nNIM = " << mhs2.NIM;
+	cout << "\nnama = " << mhs2.nama;
+	cout << "\nAlamat = " << mhs2.alamat;
+	cout << "\nUmur = " << mhs2.umur;
 
 }
